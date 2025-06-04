53ccba1a59fd160f495f24506629ea9d9d7ea5b4
Sakshi Maheshwari <149881669+sakshi-coder800@users.noreply.github.com>
2025-06-04 12:49:04 +0530

    Add files via upload

---
+++ LMS Project/frontend/src/App.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const App = () => {
+  return (
+<>
+<div className="navbar flex bg-slate-950  justify-between text-center ">
+  
+<div className="box1 flex ">
+<div className="home"></div>
+<div className="home">Go To Dashboard </div>
+<div className="dot"></div>
+
+
+</div>
+<div className="box2">
+
+<ul>
+<li>ecomm</li>
+<li>0</li>
+<li>about us </li>
+<li>contact us </li>
+<li>sign up </li>
+<li>sign in </li>
+
+</ul>
+
+</div>
+</div>
+</>
+  )
+}
+
+export default App
